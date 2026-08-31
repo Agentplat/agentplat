208683e837e6da2a55ab66cdd0bc8000b60bd6ec
grishencorp <grishencorp@gmail.com>
2026-08-31 17:46:42 -0300

    feat: establish constitutional continuity

--- packages/collective-runtime/src/morphogenesis.ts
+++ packages/collective-runtime/src/morphogenesis.ts
@@ -38,4 +38,5 @@ export * from "./morphogenesis-organizational-evolution-runtime.js";
 export * from "./morphogenesis-organizational-evolution-governance.js";
 export * from "./morphogenesis-organizational-evolution-integrations.js";
 export * from "./morphogenesis-organizational-evolution-validation.js";
+export * from "./morphogenesis-constitutional-continuity.js";
 export * from "./mission-morphogenesis.js";
